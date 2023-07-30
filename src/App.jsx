059eb06aa843b22f12e625bddcdc0b2677884167
Nguyen Ngoc Huy <ngngochuy1092@gmail.com>
2023-07-30 22:55:54 +0700

    react

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import {User} from './User'
+export default App;
+import { useState } from 'react'
+
+function App() {
+	const [color, setColor]= useState("red");
+	const changeColor= ()=> {
+		setColor(color === "red" ? "blue":"red");
+	} 
+
+	return (
+		<div>
+			<button onClick={changeColor}> Change color </button>
+			{<h1  style={{color:color}}> Hi </h1>}
+	</div>
+	);
+}
+
